8353eb53ceeadfa080c69bdf4098ec630a3afb0d
ROZAS <rozasguydavid@gmail.com>
2018-03-28 15:29:16 +0200

    vue version

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,53 @@
-// (function() {
-//     'use strict';
-    
-//     Vue.component('song-card-tmpl', {
-//         template: '<div><img src="" alt=""><h2></h2><p>eminem / curtain call</p><p>5m26</p><a class="waves-effect waves-light btn">button</a><a class="waves-effect waves-light btn">button</a><a class="waves-effect waves-light btn">button</a></div>'
-//       })
-      
-//       // créer une instance racine
-//       new Vue({
-//         el: '#song-title-cards',
-//         data: {
-//             searchResults: []
-//         },
-//         methods: {
-//             meth: function(){
-//                 // var TabResults = [];
-//                 // var MinTabResults = [];
 
-//                 // var search="https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" + $('#search').val();
-//                 // var requette = $.ajax(search);
-
-//                 // requette.done(function(reponse){
-//                 //     //$('#test').text(reponse.data);
-//                 //     //console.log(reponse.data);
-//                 //     TabResults = reponse.data;
-//                 //     MinTabResults = TabResults.slice(0,3);
-//                 //     console.log(TabResults);
-//                 // });
-//                 // requette.fail(function(){
-//                 //     alert('we got a probleme. call youston');  
-//                 // });
-//                 // this.searchResults = MinTabResults;
-//                 var TabResults = [];
-//                 var MinTabResults = [];
-//                 (function(){
-//                     $(document).ready(function(){
-//                         $('#search-btn').on('click', function(event){
-//                             event.preventDefault();    // preventDefault() empenche le navigateur de naviger vers le liens href
-//                             //console.log($('#search').val());
-//                             var search="https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" + $('#search').val();
-//                             var requette = $.ajax(search);
-
-//                             requette.done(function(reponse){
-//                                 //$('#test').text(reponse.data);
-//                                 //console.log(reponse.data);
-//                                 TabResults = reponse.data;
-//                                 MinTabResults = TabResults.slice(0,3);
-//                                 this.searchResults = MinTabResults;
-//                                 console.log(TabResults);
-//                             });
-//                             requette.fail(function(){
-//                                 alert('we got a probleme. call youston');  
-//                             });
-//                         });
-//                     });
-//                 })();
-//             }
-//         }
-//       })
-// })();
+(function() {
+    'use strict';
+      // créer une instance racine
+      var main = new Vue({                 //la variable me sert à refencer l'object
+        el: '#cardContainer',
+        data: {
+            PartUrl1:"https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=",
+            PartUrl2:"",
+            finalUrl:'',
+            option: "",
+            index: 0,
+            searchResults: []
+        },
+        filters : {
+            
+        },
+        methods: {
+            urlConstructor: function(){
+                var url = this.finalUrl;
+                if($('#search').val()===""){
+                    console.log("enter a value");
+                }else{
+                    url = this.PartUrl1 + $('#search').val();
+                    console.log(url);
+                }
+                return url;
+            },
+            urlPreviewConstructor: function(index){
+                var urlPreview = main.searchResults[index].preview;
+                console.log(main.searchResults[index].preview)
+                return urlPreview;
+            },
+            request: function(){
+                var urlFinal = this.urlConstructor();
+                var requette = $.ajax(urlFinal);
+                console.log(urlFinal);
+                requette.done(function(reponse){
+                    main.searchResults = reponse.data;
+                    console.log(main.searchResults);
+                });
+            },
+            secToMin : function(index){
+                console.log(index);
+                var time = main.searchResults[index].duration;
+                var minutes = Math.floor(time / 60);
+                var seconde = time-(minutes*60);
+                var fulltime = minutes + "m" + seconde+ "s";
+                return fulltime;
+            }
+        }
+      })
+})();
