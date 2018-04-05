01fe9065f284964040c039b811517bb65629e397
ROZAS <rozasguydavid@gmail.com>
2018-04-05 03:57:08 +0200

    version card-container component 2

--- scripts/main.js
+++ scripts/main.js
@@ -5,112 +5,11 @@
         $('.modal').modal();
       });
 
-    //   var manav ={
-    //     props : {
-    //         searchresults : Array
-    //     },
-    //     template : `<div>
-    //                     <nav>
-    //                         <div class="nav-wrapper">
-    //                             <a href="#" class="brand-logo">Logo</a>
-    //                             <ul id="nav-mobile" class="right hide-on-med-and-down">
-    //                                 <li><a href="#">Recherche</a></li>
-    //                                 <li><a href="#">Titre</a></li>
-    //                                 <li><a href="#">Album</a></li>
-    //                                 <li><a href="artist.html">Artist</a></li>
-    //                                 <li><a href="#">Favorits</a></li>
-    //                             </ul>
-    //                         </div>
-    //                     </nav>
-    //                     <nav>
-    //                         <div class="nav-wrapper">
-    //                             <form>
-    //                                 <div class="input-field">
-    //                                     <input id="search" type="search" required >
-    //                                     <button v-on:click.prevent="request()" id="search-btn">Rechercher</button>
-    //                                     <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-    //                                     <i class="material-icons">close</i>
-    //                                 </div>
-    //                             </form>
-    //                         </div>
-    //                     </nav>
-    //                 </div>`,
-    //     data : function() {
-    //         return {
-    //             PartUrl1:"https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=",
-    //             PartUrl2:"",
-    //             finalUrl:'',
-    //             // option: "",
-    //             // index: 0,
-    //             // indexSave: 0,
-    //             // searchresults: [],
-    //             // dataClick: {}
-    //         }
-    //     },
-
-    //     methods: {
-    //         urlConstructor: function(){
-    //             var url = this.finalUrl;
-    //             if($('#search').val()===""){
-    //                 console.log("enter a value");
-    //             }else{
-    //                 url = this.PartUrl1 + $('#search').val();
-    //                 console.log(url);
-    //             }
-    //             return url;
-    //         },
-    //         // urlPreviewConstructor: function(index){
-    //         //     var urlPreview = this.searchresults[index].preview;
-    //         //     return urlPreview;
-    //         // },
-    //         request: function(){
-    //             var urlFinal = this.urlConstructor();
-    //             var requette = $.ajax(urlFinal);
-    //             console.log(urlFinal);
-    //             requette.done(function(reponse){
-    //                 var results_json;
-    //                 this.searchresults = reponse.data;
-    //                 results_json = JSON.stringify(this.searchresults);               // sauvegarder dans une variable la version strigifier de mon object
-    //                 localStorage.setItem("resultsStringify",results_json);              // sauvegarder la variable dans le localStorage
-    //                 console.log(this.searchresults);
-    //                 // return this.searchresults;
-    //             });
-    //             // this.$parent.$data.searchresults = this.searchresults;
-
-    //         },
-    //         // secToMin : function(index){
-    //         //     var time = this.searchresults[index].duration;
-    //         //     var minutes = Math.floor(time / 60);
-    //         //     var seconde = time-(minutes*60);
-    //         //     var fulltime = minutes + "m" + seconde+ "s";
-    //         //     return fulltime;
-    //         // },
-    //         // indexCall : function(index){
-    //         //     var urlFinal = this.urlConstructor();
-    //         //     var requette = $.ajax(urlFinal);
-    //         //     console.log(urlFinal);
-    //         //     requette.done(function(reponse){
-    //         //         this.dataClick = reponse.data[index];
-    //         //     });
-    //         //     // this.indexSave = index;
-    //         //     // this.dataClick = this.searchresults[index];
-    //         //     // var imgLink = "url('"+ this.searchresults[index].album.cover_big +"')";
-    //         //     // $('#modal1').css({background: imgLink ,opacity:'0.5'});
-    //         //     // console.log(this.dataClick);
-    //         //     // console.log(imgLink);
-    //         // }
-    //     },
-
-    //     created : function(){
-            
-    //     },
-    //     mounted : function(){
-    //         var results_json = localStorage.getItem("resultsStringify");            // dans la variable recuper la version stringifier 
-    //         this.searchresults = JSON.parse(results_json);                          // remplacer mon objet par la sauvegarde destringifier grace a parse 
-    //     }
-    // };
-
-    var cardcontainer ={
+    var cardContainer ={
+        
+        props : {
+            searchresults : Array,
+        },
         template : `<div>
                         <div class="row">
                             <div v-for="(artist,index) in searchresults" class="col s4">
@@ -125,9 +24,9 @@
                                         <p> {{secToMin(index)}}</p>
                                     </div>
                                     <div class="card-action">
-                                        <audio controls="controls" >
+                                    <!--<audio controls="controls" >
                                             <source v-bind:src=urlPreviewConstructor(index)  type="audio/mp3 /">
-                                        </audio>
+                                        </audio>-->
 
                                         <div class="flex ">
                                             <a v-on:click="indexCall(index)" class="waves-effect waves-light btn modal-trigger" href="#modal1">Ecouter un extrait</a>
@@ -142,7 +41,12 @@
                             <div id="modal1" class="modal">
                                 <div class="modal-content">
                                     <h4>Titre : {{dataClick.title}}</h4>
-                                    <a href="#">{{dataClick.title}}</a>
+                                    <p>Album : <a href="#">{{dataClick.album.title}}</a></p>
+                                    <img :src="dataClick.artist.picture_small">
+                                    <p>Artist : <a href="#">{{dataClick.artist.name}}</a></p>
+                                    <h4>{{dataClick.title}}</h4>
+                                    <p>Durée :  {{secToMin()}} / Date de parution : </p>
+
                                 </div>
                                 <div class="modal-footer">
                                     <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
@@ -152,83 +56,121 @@
                     </div>`,
         data : function() {
             return {
-                PartUrl1:"https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=",
+                PartUrl1:"https://api.deezer.com/track/",
                 PartUrl2:"",
                 finalUrl:'',
                 option: "",
                 index: 0,
                 indexSave: 0,
-                // searchresults: [],
-                dataClick: {}
+                dataClick: {
+                    album : {},
+                    artist : {},
+                    duration : 0,
+                    explicit_lyrics: false,
+                    id : 0,
+                    link : "",
+                    preview : "",
+                    rank : 0,
+                    readable : false,
+                    title : "",
+                    title_short : "",
+                    title_version : "",
+                    type : ""
+                },
+                infoTrack : {
+                    // album : {},
+                    // artist : {} ,
+                    // available_countries : [] ,
+                    // bpm : 0 ,
+                    // contributors : [] ,
+                    // disk_number : 0 ,
+                    // duration  : 0 ,
+                    // explicit_lyrics : false ,
+                    // gain : 0 ,
+                    // id : 0 ,
+                    // isrc : "" ,
+                    // link : "" ,
+                    // preview : "" ,
+                    // rank : 0 ,
+                    // readable : "" ,
+                    // release_date : "" ,
+                    // share : "" ,
+                    // title : "" ,
+                    // title_short : "" ,
+                    // title_version : "" ,
+                    // track_position : 0 ,
+                    // type : ""
+                }
             }
         },
 
         methods: {
-            // urlConstructor: function(){
-            //     var url = this.finalUrl;
-            //     if($('#search').val()===""){
-            //         console.log("enter a value");
-            //     }else{
-            //         url = this.PartUrl1 + $('#search').val();
-            //         console.log(url);
-            //     }
-            //     return url;
-            // },
+            urlConstructorTrack: function(){
+                var url = this.finalUrl;
+                url = this.PartUrl1 + this.dataClick.id;
+                console.log(url);
+                return url;
+            },
             urlPreviewConstructor: function(index){
                 var urlPreview = this.searchresults[index].preview;
                 return urlPreview;
             },
-            // request: function(){
-            //     var urlFinal = this.urlConstructor();
-            //     var requette = $.ajax(urlFinal);
-            //     console.log(urlFinal);
-            //     requette.done(function(reponse){
-            //         var results_json;
-            //         this.searchresults = reponse.data;
-            //         results_json = JSON.stringify(this.searchresults);               // sauvegarder dans une variable la version strigifier de mon object
-            //         localStorage.setItem("resultsStringify",results_json);              // sauvegarder la variable dans le localStorage
-            //         console.log(this.searchresults);
-            //     });
-            // },
-            secToMin : function(index){
-                var time = this.searchresults[index].duration;
+            secToMin : function(){
+                // var time = this.searchresults[index].duration;
+                // var minutes = Math.floor(time / 60);
+                // var seconde = time-(minutes*60);
+                // var fulltime = minutes + "m" + seconde+ "s";
+                // return fulltime;
+
+                var time = this.dataClick.duration;
                 var minutes = Math.floor(time / 60);
                 var seconde = time-(minutes*60);
                 var fulltime = minutes + "m" + seconde+ "s";
                 return fulltime;
             },
             indexCall : function(index){
-                var urlFinal = this.urlConstructor();
-                var requette = $.ajax(urlFinal);
-                console.log(urlFinal);
-                requette.done(function(reponse){
-                    this.dataClick = reponse.data[index];
-                });
-                // this.indexSave = index;
-                // this.dataClick = this.searchresults[index];
+                this.dataClick = this.searchresults[index];
                 // var imgLink = "url('"+ this.searchresults[index].album.cover_big +"')";
                 // $('#modal1').css({background: imgLink ,opacity:'0.5'});
-                // console.log(this.dataClick);
-                // console.log(imgLink);
+                this.infoTrackCall(this.getReponse);
+                // this.infoTrack = this.infoTrackCall();
+                
+            },
+            infoTrackCall : function(getReponse){
+                var urlFinal = this.urlConstructorTrack();
+                $.ajax({
+                    url : urlFinal,
+                    dataType : 'jsonp', // <-- Informe jQuery qu'il recevra du JSONP de la part de Deezer
+                    data : {
+                        output : 'jsonp', // <-- Indique à Deezer que le format de retour doit être du JSONP
+                    },
+                    success: function( reponse ) {
+                        getReponse(reponse)
+                        return reponse;
+                    },
+                    errorCallback: function( reponse ) {
+                        alert("votre recherche n'as pas aboutie à un resultat");
+                    }
+                });
+            },
+            getReponse : function(reponse){
+                console.log(reponse);
+                this.infoTrack = reponse;
             }
         },
 
-        created : function(){
+        // created : function(){
             
-        },
-        props : {
-            searchresults : Array,
-        }
+        // }
     };
 
- new Vue({                 //la variable me sert à refencer l'object
+ var elApp = new Vue({                 //la variable me sert à refencer l'object
         el: '#app',
         components : { 
-            'cardcontainer' : cardcontainer ,
-            // 'manav' : manav        
+            'card-container' : cardContainer ,    
         },
         data : {
-                PartUrl1:"https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=",
+                PartUrl1:"https://api.deezer.com/search?q=",
                 PartUrl2:"",
                 finalUrl:'',
                 option: "",
@@ -253,29 +195,22 @@
             },
             request: function(){
                 var urlFinal = this.urlConstructor();
-                var requette = $.ajax(urlFinal);
-                console.log(urlFinal);
-                requette.done(function(reponse){
-                    var results_json;
-                    this.searchresults = reponse.data;
-                    results_json = JSON.stringify(this.searchresults);               // sauvegarder dans une variable la version strigifier de mon object
-                    localStorage.setItem("resultsStringify",results_json);              // sauvegarder la variable dans le localStorage
-                    console.log(this.searchresults);
-                });
-                var results_json = localStorage.getItem("resultsStringify");            // dans la variable recuper la version stringifier 
-                this.searchresults = JSON.parse(results_json);                          // remplacer mon objet par la sauvegarde destringifier grace a parse 
-
+                $.ajax({
+                    url : urlFinal,
+                    dataType : 'jsonp', // <-- Informe jQuery qu'il recevra du JSONP de la part de Deezer
+                    data : {
+                        output : 'jsonp', // <-- Indique à Deezer que le format de retour doit être du JSONP
+                    },
+                    success: function( reponse ) {
+                        elApp.searchresults = reponse.data;
+                        console.log(this.searchresults);
+                        console.log( reponse );
+                    },
+                    errorCallback: function( reponse ) {
+                        alert("votre recherche n'as pas aboutie à une resultat");
+                    }
+                })
             }
         },
-        // mounted : function(){
-        //     var results_json = localStorage.getItem("resultsStringify");            // dans la variable recuper la version stringifier 
-        //     if (results_json === null){
-        //         this.searchresults = [];
-        //     }
-        //     else{
-        //         this.searchresults = JSON.parse(results_json);                       // remplacer mon objet par la sauvegarde destringifier grace a parse 
-        //     }
-            
-        // }
       })
 })();
